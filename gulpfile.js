b14de44c438d4b675060057614bbf461da75e69d
Call Levels <calllevels@BlastOuts-MacBook-Pro.local>
2015-03-10 17:27:56 +0800

    Added Fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
+// Save a reference to the `reload` method
+var reload = browserSync.reload;
 
 // Static Server + watching scss/html files
 gulp.task('serve', function() {
@@ -9,7 +11,7 @@ gulp.task('serve', function() {
         server: "./app"
     });
 
-    gulp.watch("app/css/*.css").on('change', reload);
+    gulp.watch("app/assets/css/*.css").on('change', reload);
     gulp.watch("app/*.html").on('change', reload);
 });
 
